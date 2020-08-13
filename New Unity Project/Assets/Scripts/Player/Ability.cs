56bfc509298bccb913d10d87b4ca3756d5544d93
Inviser <inviser7@gmail.com>
2020-08-13 16:08:11 +0300

    fix merge conflicts

--- New Unity Project/Assets/Scripts/Player/Ability.cs
+++ New Unity Project/Assets/Scripts/Player/Ability.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< HEAD
-using UnityEngine;
-
-public abstract class Ability : MonoBehaviour
-{
-=======
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -17,7 +11,6 @@ public abstract class Ability : MonoBehaviour
     public Sprite skillSprite;
     public UnityEvent Casted;
 
->>>>>>> КАК-ДВИГАТЬСЯ-БЛЯТЬ
     private const float timeEpsilon = 0.0001f;
     public void Cast()
     {
@@ -26,15 +19,6 @@ public abstract class Ability : MonoBehaviour
             DoCast();
             //isAvaible = false;
             nextUseTime = Time.time + coolDown;
-<<<<<<< HEAD
-        }
-    }
-
-    protected abstract void DoCast();
-    //public bool isAvaible { get; set; } = true;
-    public abstract float coolDown { get; }
-
-=======
             Casted.Invoke();
             
         }
@@ -46,17 +30,12 @@ public abstract class Ability : MonoBehaviour
     public abstract float coolDown { get; }
     public abstract string[] axises { get; }
     
->>>>>>> КАК-ДВИГАТЬСЯ-БЛЯТЬ
     private void Awake()
     {
         nextUseTime = Time.time;
     }
 
     public float nextUseTime { get; set; }
-<<<<<<< HEAD
-
-=======
     
     //protected abstract Sprite skillImage { get; }
->>>>>>> КАК-ДВИГАТЬСЯ-БЛЯТЬ
 }
