7d18f4097fb95a5ebcb42b1be4f2123e8838cd1c
sonomalik <68858564+sonomalik@users.noreply.github.com>
2020-08-27 01:07:01 +0200

    Ragdoll & katana trails.

---
+++ TopDownProject/Assets/Scripts/NPC/RagdollController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        EnableRagdoll(false);
+    }
+
+    public void StartRagdolling()
+    {
+        EnableRagdoll(true);
+    }
+
+    private void EnableRagdoll(bool enable)
+    {
+        foreach (Rigidbody item in GetComponentsInChildren<Rigidbody>())
+        {
+            item.isKinematic = !enable;
+            Collider collider = item.GetComponent<Collider>();
+            if (collider) collider.enabled = enable;
+        }
+    }
+
+}
