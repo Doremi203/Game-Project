389c23a5d4b5a65af7cc2e92e9a85af05db0c458
sonomalik <68858564+sonomalik@users.noreply.github.com>
2020-08-27 19:49:49 +0200

    Фиксы Ragdoll'а.

--- TopDownProject/Assets/Scripts/ScriptableObjects/Level.cs
+++ TopDownProject/Assets/Scripts/ScriptableObjects/Level.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Callbacks;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "New Level", menuName = "Level")]
 public class Level : ScriptableObject
